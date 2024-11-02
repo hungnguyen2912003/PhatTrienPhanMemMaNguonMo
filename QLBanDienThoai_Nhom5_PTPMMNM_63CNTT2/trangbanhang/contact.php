17face652e94f8a8a6c51b21204d744aebb0068b
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2024-11-02 15:49:47 +0700

    init

---
+++ QLBanDienThoai_Nhom5_PTPMMNM_63CNTT2/trangbanhang/contact.php
@@ -0,0 +1,15 @@
+<?php
+$base_url = "/PhatTrienPhanMemMaNguonMo/QLBanDienThoai_Nhom5_PTPMMNM_63CNTT2";
+$title = "Trang liên hệ MEGATECH";
+?>
+
+<?php
+include('includes/header.html');
+?>
+
+<div class="main-panel">
+    <?php
+        include ('contact.html'); ?>
+</div>
+
+<?php include 'includes/footer.html'; ?>
