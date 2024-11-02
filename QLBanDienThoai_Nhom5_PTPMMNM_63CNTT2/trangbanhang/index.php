00dc16f41ad4e6e98ba175e48d4f7570231f2bb6
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2024-11-02 13:49:10 +0700

    init

---
+++ QLBanDienThoai_Nhom5_PTPMMNM_63CNTT2/trangbanhang/index.php
@@ -0,0 +1,16 @@
+<?php
+$base_url = "/PhatTrienPhanMemMaNguonMo/QLBanDienThoai_Nhom5_PTPMMNM_63CNTT2";
+$title = "Trang bán hàng MEGATECH";
+?>
+
+<?php
+include('includes/header.html');
+?>
+
+<div class="main-panel">
+    <?php
+        include ('home.html');
+    ?>
+</div>
+
+<?php include 'includes/footer.html'; ?>
