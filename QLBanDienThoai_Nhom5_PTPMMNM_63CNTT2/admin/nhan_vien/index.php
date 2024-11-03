61c9743db8634f3c502f9163eb176f9e83f898fd
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2024-11-03 09:56:57 +0700

    init

--- QLBanDienThoai_Nhom5_PTPMMNM_63CNTT2/admin/nhan_vien/index.php
+++ QLBanDienThoai_Nhom5_PTPMMNM_63CNTT2/admin/nhan_vien/index.php
@@ -4,6 +4,12 @@ $base_url = "/PhatTrienPhanMemMaNguonMo/QLBanDienThoai_Nhom5_PTPMMNM_63CNTT2";
 include('../includes/header.html');
 include ('../_PartialSideBar.html');
 include('../includes/footer.html');
+
+$connect = mysqli_connect("localhost", "root", "", "qlbandienthoai")
+OR die ('Không thể kết nối MySQL: ' . mysqli_connect_error());
+
+$sql = "SELECT * FROM nhan_vien";
+$result = mysqli_query($connect, $sql);
 ?>
 
 <!DOCTYPE html>
@@ -12,89 +18,65 @@ include('../includes/footer.html');
     <meta charset="UTF-8">
     <title>Quản lý nhân viên</title>
 </head>
-
 <body>
 <style>
     .custom-textbox {
         height: 50px;
-        border: 2px solid #0094ff
+        border: 2px solid #0094ff;
     }
 </style>
 <div class="main-panel">
     <div class="content">
         <div class="page-inner">
             <div class="page-header">
-                <div class="col-md-12">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <h4 class="page-title">Danh sách nhân viên</h4>
-                        </div>
-                        <div class="col-md-6 text-right">
-                            <ul class="breadcrumbs">
-                                <li class="nav-home">
-                                    <a href="<?php echo $base_url?>/admin/index.php">
-                                        <i class="flaticon-home"></i>
-                                    </a>
-                                </li>
-                                <li class="separator">
-                                    <i class="flaticon-right-arrow"></i>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="<?php echo $base_url?>/admin/nhan_vien/index.php">Danh sách nhân viên</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+                <!-- Nội dung header -->
             </div>
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
-                        <div class="row">
-                            <div class="col-md-4">
-                                <div class="card-tools">
-                                    <a href="<?php echo $base_url?>/admin/nhan_vien/create.php" class="btn btn-rounded btn-primary">Thêm mới</a>
-                                </div>
-                            </div>
-                            <div class="col-md-5">
-                                <div class="input-group input-group-sm">
-
-                                    <input type="text" name="Searchtext" class="form-control custom-textbox" placeholder="Nhập thông tin nhân viên bạn muốn tìm kiếm">
-                                    <span class="input-group-append">
-                                        <button type="submit" class="btn btn-info btn-flat">Tìm kiếm</button>
-                                  </span>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="card-tools text-right">
-                                    <a href="#" class="btn btn-rounded btn-danger"><i class="fa fa-trash" style="font-size: 15px;"></i> Thùng rác</a>
-                                </div>
-                            </div>
-
-                        </div>
+                        <!-- Nội dung card-header -->
                     </div>
                     <div class="card-body">
                         <div class="message-container">
-                            <!--Hiển thị dòng thông báo-->
+                            <!-- Hiển thị dòng thông báo -->
                         </div>
                         <div class="table-responsive">
                             <table id="multi-filter-select" class="display table table-striped table-hover table-bordered tableNhanVien">
                                 <thead>
                                 <tr class="text-center">
                                     <th><input type="checkbox" id="SelectAll" /></th>
-                                    <th>#</th>
+                                    <th>STT</th>
                                     <th>Mã nhân viên</th>
                                     <th>Họ tên nhân viên</th>
                                     <th>Hình ảnh</th>
                                     <th>Ngày sinh</th>
                                     <th>Giới tính</th>
                                     <th>Số điện thoại</th>
-                                    <th>Chức vụ</th>
                                     <th>Chức năng</th>
                                 </tr>
                                 </thead>
                                 <tbody>
-
+                                <?php
+                                $stt = 1;
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    echo "<tr>";
+                                    echo "<td class='text-center'><input type='checkbox' /></td>";
+                                    echo "<td class='text-center'>$stt</td>";
+                                    echo "<td class='text-center'>{$row['id']}</td>";
+                                    echo "<td class='text-center'>{$row['hoTen']}</td>";
+                                    echo "<td class='text-center'><img width='80' src='$base_url/Images/{$row['Images']}'/></td>";
+                                    echo "<td class='text-center'>" . date("d/m/Y", strtotime($row['ngaySinh'])) . "</td>";
+                                    echo "<td class='text-center'>" . ($row['gioiTinh'] == 1 ? 'Nam' : 'Nữ') . "</td>";
+                                    echo "<td class='text-center'>{$row['soDienThoai']}</td>";
+                                    echo "<td class='text-center'>
+                                                <a href='$base_url/admin/nhanvien/detail?manv={$row['id']}' class='btn btn-xs btn-warning text-white'><i class='fa-solid fa-circle-info'></i></a>
+                                                <a href='$base_url/admin/nhanvien/edit?manv={$row['id']}' class='btn btn-xs btn-primary'><i class='fa-solid fa-pen-to-square'></i></a>
+                                                <a href='$base_url/admin/nhanvien/delete?manv={$row['id']}' class='btn btn-xs btn-danger'><i class='fa-solid fa-trash-can'></i></a>
+                                              </td>";
+                                    echo "</tr>";
+                                    $stt++;
+                                }
+                                ?>
                                 </tbody>
                             </table>
                         </div>
@@ -106,4 +88,3 @@ include('../includes/footer.html');
 </div>
 </body>
 </html>
-
