a8f29b9e444b8e2988ac09ab6d4edf10830b55e5
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2024-11-12 14:50:18 +0700

    init

--- QLBanDienThoai_Nhom5_PTPMMNM_63CNTT2/admin/index.php
+++ QLBanDienThoai_Nhom5_PTPMMNM_63CNTT2/admin/index.php
@@ -12,5 +12,21 @@ if (!isset($_SESSION['logged']) || !$_SESSION['logged']) {
     include('home.html');
     echo "</div>";
     include('includes/footer.html');
+
+    $hoTenNV = $_SESSION['hoTen'] ?? null;
+    // Kiểm tra nếu thông báo chào mừng chưa hiển thị
+    $showWelcome = false;
+    if (isset($_SESSION['logged_kh']) && $_SESSION['logged_kh'] === true && !isset($_SESSION['welcome_shown'])) {
+        $showWelcome = true;
+        $_SESSION['welcome_shown'] = true; // Đánh dấu đã hiển thị thông báo
+    }
 }
 ?>
+<?php if ($showWelcome): ?>
+    <!-- Modal chào mừng người dùng -->
+    <script>
+        $(document).ready(function() {
+            alert("Chào mừng <?php echo $hoTenNV; ?> đến với Shop bán điện thoại MEGATECH");
+        });
+    </script>
+<?php endif; ?>
--- QLBanDienThoai_Nhom5_PTPMMNM_63CNTT2/trangbanhang/timeOutSession.php
+++ QLBanDienThoai_Nhom5_PTPMMNM_63CNTT2/trangbanhang/timeOutSession.php
@@ -5,24 +5,6 @@ $base_url = "/PhatTrienPhanMemMaNguonMo/QLBanDienThoai_Nhom5_PTPMMNM_63CNTT2";
 // SESSION_TIMEOUT được định nghĩa là 600 giây (10 phút)
 const SESSION_TIMEOUT = 600;
 
-//Kiểm tra phiên đăng nhập của nhân viên: Nếu chưa hoặc false thì chuyển đến login
-if (!isset($_SESSION['logged']) || !$_SESSION['logged']) {
-    header("Location: $base_url/admin/login.php");
-    exit;
-}
-else{
-    // Kiểm tra thời gian hết hạn phiên của nhân viên
-    if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > SESSION_TIMEOUT)) {
-        // Nếu hết thời gian, chuyển hướng đến trang đăng nhập với thông báo timeout
-        session_unset();
-        session_destroy();
-        header("Location: $base_url/admin/login.php?timeout=true");
-        exit;
-    }
-    // Cập nhật thời gian hoạt động cuối cùng
-    $_SESSION['last_activity'] = time();
-}
-
 // Kiểm tra thời gian hết hạn phiên của khách hàng
 if (isset($_SESSION['logged_kh']) && $_SESSION['logged_kh'] === true) {
     if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > SESSION_TIMEOUT)) {
