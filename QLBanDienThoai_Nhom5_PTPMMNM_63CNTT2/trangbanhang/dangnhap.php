eb425cb9168c138dcf6fb601d54e5dd1b61f9556
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2024-11-15 00:43:39 +0700

    init

--- QLBanDienThoai_Nhom5_PTPMMNM_63CNTT2/trangbanhang/dangnhap.php
+++ QLBanDienThoai_Nhom5_PTPMMNM_63CNTT2/trangbanhang/dangnhap.php
@@ -73,7 +73,7 @@ if (isset($_POST['dangNhap'])) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Trang đăng nhập á</title>
+    <title>Trang đăng nhập</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
