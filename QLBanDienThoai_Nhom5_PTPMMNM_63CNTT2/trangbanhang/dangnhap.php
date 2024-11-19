65cc07c6ae554b69128c747aa1d0bc53bc3ab4e1
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2024-11-19 18:08:10 +0700

    init

--- QLBanDienThoai_Nhom5_PTPMMNM_63CNTT2/trangbanhang/dangnhap.php
+++ QLBanDienThoai_Nhom5_PTPMMNM_63CNTT2/trangbanhang/dangnhap.php
@@ -113,7 +113,9 @@ if (isset($_POST['dangNhap'])) {
                 <fieldset>
                     <div class="form-group form-group-default">
                         <label>Tên tài khoản <span class="text-danger">*</span></label>
-                        <input class="form-control" placeholder="Tên đăng nhập" name="username" type="text" autofocus value="<?php echo $_POST['username'] ?? ''; ?>"/>
+                        <label>
+                            <input class="form-control" placeholder="Tên đăng nhập" name="username" type="text" autofocus value="<?php echo $_POST['username'] ?? ''; ?>"/>
+                        </label>
                     </div>
                     <div class="form-group form-group-default">
                         <label>Mật khẩu <span class="text-danger">*</span></label>
